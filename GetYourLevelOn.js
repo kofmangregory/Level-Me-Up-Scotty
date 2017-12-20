f32c31c77691ba56efd3b77a27d3d9829e572c1f
Gregory <kofmangregory@gmail.com>
2017-12-20 00:10:20 -0500

    Added GetYourLevelOn.js

---
+++ GetYourLevelOn.js
@@ -0,0 +1,10 @@
+var level = require('level');
+
+module.exports = function (databaseDir, key, callback) {
+	var db = level(databaseDir);
+	db.get(key, function(error, value) {
+		db.close(function(err) {
+			callback(err || error, value);
+		})
+	});
+}
